fe00fa954720e9be20453e2d7bf790dca1c86132
ofefo <fefobrandao@gmail.com>
2020-04-27 18:58:49 -0300

    Add files

---
+++ bach.py
@@ -0,0 +1,19 @@
+import random
+import urllib.request
+from bs4 import BeautifulSoup
+
+with open('bwv.txt') as f:
+    bwv = f.readlines()
+
+n = random.randint(1, 1127)
+
+print("Today's Bach is: \n ", bwv[n])
+
+textToSearch = bwv[n]
+query = urllib.parse.quote(textToSearch)
+url = "https://www.youtube.com/results?search_query=" + query
+response = urllib.request.urlopen(url)
+html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
+for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    print('https://www.youtube.com' + vid['href'])
