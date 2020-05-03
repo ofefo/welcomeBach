fb52880e584880c2114d4beee43c911c3e8eab6b
ofefo <fefobrandao@gmail.com>
2020-05-03 18:46:35 -0300

    Restrict printing to 5 links

--- bach.py
+++ bach.py
@@ -3,7 +3,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 with open('bwv.txt') as f:
-    bwv = f.readlines()
+	bwv = f.readlines()
 
 n = random.randint(1, 1127)
 
@@ -15,5 +15,12 @@ url = "https://www.youtube.com/results?search_query=" + query
 response = urllib.request.urlopen(url)
 html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
-for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
-    print('https://www.youtube.com' + vid['href'])
+
+def printLinks():
+	linkList = []
+	for i in range(5):
+		for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+			v = 'https://www.youtube.com' + vid['href']
+		print(v)
+
+printLinks()
