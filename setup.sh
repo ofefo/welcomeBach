9fb7820d206875206420f8614feef21909abdc1f
ofefo <fefobrandao@gmail.com>
2020-05-23 15:23:35 -0300

    Add setup and bash file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m venv venv;
+dir=$pwd/venv/bin;
+. .$dir/activate;
+pip3 install -r requirements.txt;
+deactivate;
+chmod +x "welcomeBach.sh";
+cp gnome-terminal.desktop $HOME/.config/autostart/welcomeBach.desktop;
