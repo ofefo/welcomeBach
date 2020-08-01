0ed387b6a65d2dac8e7bd8965d137b3ab89530d4
ofefo <fefobrandao@gmail.com>
2020-08-01 18:02:30 -0300

    New version

--- bach.py
+++ bach.py
@@ -1,26 +1,51 @@
+from sys import argv
 import random
 import urllib.request
 import datetime
 import re
 from collections import OrderedDict
 
+#if no argument is passed, prints 3 links as default
+if len(argv) < 2:
+    number = 3
+else:
+    number = int(argv[1])
+
+#set the seed for random as the difference in days from today and January 1st, 2020
 start = datetime.datetime(2020, 1, 1, 0, 0, 0, 0)
 end = datetime.datetime.utcnow()
 diff = abs((end - start).days)
+random.seed(diff)
 
+#choose today's piece
 with open('bwv.txt') as f:
     bwv = f.readlines()
 
-random.seed(diff)
 n = random.randint(1, 1105)
-print("Today's Bach is: \n ", bwv[n])
+piece = bwv[n] 
 
-textToSearch = bwv[n]
+#if it has already been heard, get the next one on the list
+with open('listened.txt', 'r+') as f:
+    while True:
+        for line in f:
+            if piece in line:
+                n += 1
+                piece = bwv[n]
+                continue
+        else:
+            f.write(piece)
+            break
+
+print("Today's Bach is: \n ", piece)
+
+#looks for it on youtube
+textToSearch = piece
 query = urllib.parse.quote(textToSearch)
 url = "https://www.youtube.com/results?search_query=" + query
 html = urllib.request.urlopen(url).read().decode('utf-8')
 pattern = re.compile('videoId":"(\w{11})"')
 
+#print N video links based on input argument
 def videoFinder(n):
     matches = []
     for vid in re.findall(pattern, html):
@@ -30,4 +55,4 @@ def videoFinder(n):
     for i in range(n):
         print(links[i])
 
-videoFinder(3)
+videoFinder(number)
