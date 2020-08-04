8cf9e2055ef0a8b62f0f895d468c02454a5ca839
ofefo <fefobrandao@gmail.com>
2020-08-04 14:17:47 -0300

    Improve install script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-EXE_PATH=$HOME/.local/bin
+if [ -d "$HOME/.local/bin" ]; then
+	EXE_PATH=$HOME/.local/bin
+else
+	EXE_PATH=/usr/local/bin
+fi
 mkdir -p $EXE_PATH
 sed "s|<PWD>|"${PWD}"|" ${PWD}/Bach > $EXE_PATH"/Bach"
 chmod +x $EXE_PATH"/Bach"
-if [ $1="gnome" ]
-then
+if [ $1="gnome" ]; then
     mkdir -p $HOME/.config/autostart
     cp welcomeBach.desktop $HOME/.config/autostart/welcomeBach.desktop
 fi
