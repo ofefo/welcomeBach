664eb17aec5c0f86dd9a6db13e081957c7e4ba2e
ofefo <fefobrandao@gmail.com>
2020-08-04 14:30:01 -0300

    Add echo and listened install

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 if [ -d "$HOME/.local/bin" ]; then
 	EXE_PATH=$HOME/.local/bin
 else
+	echo "Can't install in your home folder! Need permission to do it in /usr/, try again with sudo!"
 	EXE_PATH=/usr/local/bin
 fi
+touch listened.txt
 mkdir -p $EXE_PATH
 sed "s|<PWD>|"${PWD}"|" ${PWD}/Bach > $EXE_PATH"/Bach"
 chmod +x $EXE_PATH"/Bach"
