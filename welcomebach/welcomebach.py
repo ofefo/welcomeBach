53467b349cbc8a6f0bebc4053ce9c65fb5d17d5c
ofefo <fefobrandao@gmail.com>
2020-12-22 20:19:49 -0300

    Rename downloader, add command to execute with welcomebach, update dict

--- welcomebach/welcomebach.py
+++ welcomebach/welcomebach.py
@@ -10,67 +10,11 @@ with open('dict.csv', newline='') as f:
 keys = list(catalogue.keys())
 
 parser = argparse.ArgumentParser(description='welcomeBach: Your daily dose of counterpoint')
-parser.add_argument('-v', 
-        help='Increase/decrease verbosity (default = 3)', 
-        type=int, 
-        default=3)
-parser.add_argument('-r',
-        help='Relisten to the last piece',
-        action='store_true',
-        default=False)
-parser.add_argument('-c',
-        choices=keys,
-        help='Choose the composer you want to listen to',
-        type=str,
-        default='Bach')
+parser.add_argument('-v', help='Increase/decrease verbosity (default = 3)', type=int, default=3)
+parser.add_argument('-r', help='Relisten to the last piece', action='store_true', default=False)
+parser.add_argument('-c', choices=keys, help='Choose the composer you want to listen to', type=str, default='Bach')
+parser.add_argument('-d', help='Download a new catalogue', action='store_true', default=False)
 args = parser.parse_args()
-
-#if no argument is passed, print 3 links as default
-number = args.v
-
-#if no argument is passed, Bach is the desired composer
-composer = args.c
-composer = composer.title()
-
-#set the seed for random as the difference in days from today and January 1st, 2020
-start = datetime.datetime(2020, 1, 1, 0, 0, 0, 0)
-end = datetime.datetime.utcnow()
-diff = abs((end - start).days)
-random.seed(diff)
-
-#set .txt file name according to dictionary
-chdir('../catalogues')
-txtName = str(catalogue[composer])
-
-#choose today's opus
-with open(txtName) as f:
-    opus = f.readlines()
-    count = len(opus)
-n = random.randint(1, count)
-piece = opus[n] 
-
-#if passed with argv -r, for "relisten", get the last item from the listened list
-#else, check if it has already been heard, if so, choose another pice 
-chdir('../listens')
-listened = str(catalogue[composer] + '_listens')
-if args.r == True:
-    with open(listened, 'r') as f:
-        for line in f:
-            pass
-        piece = line
-else:
-    with open(listened, 'r+') as f:
-        while True:
-            for line in f:
-                if piece in line:
-                    n = random.randint(1, count)
-                    piece = opus[n]
-                    continue
-            else:
-                f.write(piece)
-                break
-
-print("Today's %s is: \n " % composer, piece)
 
 #print N video links based on input argument
 def videoFinder(n, composer, piece):
@@ -86,4 +30,61 @@ def videoFinder(n, composer, piece):
     for i in range(n):
         print(links[i])
 
-videoFinder(number, composer, piece)
+
+def main():
+    
+    if not args.d:
+
+        #if no argument is passed, print 3 links as default
+        number = args.v
+        
+        #if no argument is passed, Bach is the desired composer
+        composer = args.c
+        composer = composer.title()
+        
+        #set the seed for random as the difference in days from today and January 1st, 2020
+        start = datetime.datetime(2020, 1, 1, 0, 0, 0, 0)
+        end = datetime.datetime.utcnow()
+        diff = abs((end - start).days)
+        random.seed(diff)
+        
+        #set .txt file name according to dictionary
+        chdir('../catalogues')
+        txtName = str(catalogue[composer])
+        
+        #choose today's opus
+        with open(txtName) as f:
+            opus = f.readlines()
+            count = len(opus)
+        n = random.randint(1, count)
+        piece = opus[n] 
+        
+        #if passed with argv -r, for "relisten", get the last item from the listened list
+        #else, check if it has already been heard, if so, choose another pice 
+        chdir('../listens')
+        listened = str(catalogue[composer] + '_listens')
+        if args.r == True:
+            with open(listened, 'r') as f:
+                for line in f:
+                    pass
+                piece = line
+        else:
+            with open(listened, 'r+') as f:
+                while True:
+                    for line in f:
+                        if piece in line:
+                            n = random.randint(1, count)
+                            piece = opus[n]
+                            continue
+                    else:
+                        f.write(piece)
+                        break
+        
+        print("Today's %s is: \n " % composer, piece)
+        return videoFinder(number, composer, piece)
+    else:
+       system('python3 catalogue_dl.py')
+    
+
+if __name__ == '__main__':
+    main()
