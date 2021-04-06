a659770027b5371816c8f906e598f5a9d5aff01a
ofefo <fefobrandao@gmail.com>
2021-04-06 00:38:31 -0300

    Improve install

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ deactivate;
 
 if [ -d "$HOME/.local/bin" ]; then
 	EXE_PATH=$HOME/.local/bin
-	SUCCESS=1
+	SUCCESS=true
 else
 	if [ ! "$(whoami)" == "root" ]; then
 		echo "Can't install in your home folder! Need permission to do it in /usr/, try again with sudo!"
 		exit
 	else
 		EXE_PATH=/usr/local/bin
-		SUCCESS=1
+		SUCCESS=true
 	fi
 fi
 sed "s|<PWD>|"${PWD}"|" ${PWD}/welcomebach/welcomebach > $EXE_PATH"/welcomebach"
@@ -23,6 +23,6 @@ if [ $1="gnome" ]; then
     mkdir -p $HOME/.config/autostart
     cp ./welcomebach/welcomeBach.desktop $HOME/.config/autostart/welcomeBach.desktop
 fi
-if [ $SUCCESS == 1 ]; then
+if $SUCCESS; then
 	echo "Done! You're ready to get polyphonic."
 fi
